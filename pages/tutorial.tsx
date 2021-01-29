8630f06bfe7ef4ce53b472b2f5645cda7db29e44
ishank <ishank.web.dev@gmail.com>
2021-01-29 17:25:26 +0530

    build fix

--- pages/tutorial.tsx
+++ pages/tutorial.tsx
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
-import { useTransition, animated } from 'react-spring';
-import { WELCOME_TEXT } from '../constants';
-import AnimatedMessage from '../components/Tutorial/_AnimatedMessage';
-import CommandList from '../components/Tutorial/CommandList';
-import BackButton from '../components/shared/BackButton';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import { useTransition, animated } from "react-spring";
+import { WELCOME_TEXT } from "../constants";
+import AnimatedMessage from "../components/Tutorial/_AnimatedMessage";
+import CommandList from "../components/Tutorial/CommandList";
+import BackButton from "../components/shared/BackButton";
 
 const StyledWelcome = styled(animated.pre)`
   font-size: 7pt;
@@ -15,20 +15,11 @@ const StyledWelcome = styled(animated.pre)`
 `;
 
 const Tutorial = () => {
-  const transitions = useTransition([WELCOME_TEXT], (item) => item.key, {
-    from: { transform: 'translate3d(0,-40px,0)' },
-    enter: { transform: 'translate3d(0,0px,0)' },
-    leave: { transform: 'translate3d(0,-40px,0)' },
-  });
-
   return (
     <>
       <BackButton prevLink="" />
-      {transitions.map(({ item, props, key }) => (
-        <StyledWelcome style={props} key={key}>
-          {item}
-        </StyledWelcome>
-      ))}
+      <StyledWelcome>{WELCOME_TEXT[0]}</StyledWelcome>
+
       <AnimatedMessage />
       <CommandList />
     </>
--- plugins/MKDIR.tsx
+++ plugins/MKDIR.tsx
@@ -1,10 +1,16 @@
-import { FSStore } from '../types';
+import { FSStore } from "../types";
 
-const MKDIR = (fsStore: FSStore, args:string[] = []): string => {
+const MKDIR = (fsStore: FSStore, args: string[] = []): string => {
   const { mkdir, currentDir } = fsStore;
   const [pathToDir] = args;
 
   mkdir(pathToDir);
+  if (!("indexedDB" in window)) {
+    console.log(
+      "This browser doesn't support IndexedDB storage will fail save your file instead"
+    );
+    return;
+  }
   return `creating directory ${pathToDir} in ${currentDir}`;
   // debugger;
 };
