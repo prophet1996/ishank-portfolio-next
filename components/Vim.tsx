5fbd8d38ba5fbf5665de99b51db7f7cf07d5ab87
ishank <ishank.web.dev@gmail.com>
2021-01-29 17:12:22 +0530

    vim wokring

--- components/Vim.tsx
+++ components/Vim.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { useCallback } from "react";
 import { Vim } from "react-vim-wasm";
+
 const VimEditor = () => {
-  const onVimExit = useCallback(
-    (s) => alert(`Vim exited with status ${s}`),
-    []
-  );
+  const onVimExit = () => {};
   const onFileExport = useCallback(
     (f, buf) => console.log("file exported:", f, buf),
     []
@@ -14,10 +12,10 @@ const VimEditor = () => {
 
   return (
     <Vim
-      worker="/static/vim-wasm"
+      worker="static/vim.js"
       onVimExit={onVimExit}
+      style={{ height: "100vh", width: "100vw" }}
       onFileExport={onFileExport}
-      readClipboard={navigator.clipboard && navigator.clipboard.readText}
       onWriteClipboard={navigator.clipboard && navigator.clipboard.writeText}
       onError={onError}
     />
---
+++ next.config.js
@@ -0,0 +1,6 @@
+const withTM = require("next-transpile-modules")([
+  "react-vim-wasm",
+  "vim-wasm",
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM();
